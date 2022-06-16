2115fd77522626a33c409b641d3af8e9305c9750
qinzhen <qinzhen@sensetime.com>
2022-06-16 17:12:37 +0800

    update

---
+++ set_env.sh
@@ -0,0 +1,6 @@
+export CUDA_HOME='/mnt/lustre/share/cuda-10.2'
+export PATH="/mnt/lustre/share/cuda-10.2/bin:$PATH"
+export LD_LIBRARY_PATH="/mnt/lustre/share/cuda-10.2/lib64/:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="/mnt/lustre/share/cuda-10.2/lib64/:$LIBRARY_PATH"
+
+# export DATA_PATH=
