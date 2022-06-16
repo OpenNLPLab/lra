f0995506c5eca49da9f8010378ecf9530b2c8e08
hanxiaodong <hanxiaodong@sensetime.com>
2022-06-16 23:29:20 +0800

    optimize submission method

---
+++ run_tasks.py
@@ -0,0 +1,137 @@
+import os
+import time
+import sys
+
+tasks = ["imdb","cifar","listops","pathfinder","aan"]
+archs = ["flash","flash_linear","lg","ls","performer"]
+
+flash_args={'cifar':[1024,128],"imdb":[4096,128],"listops":[2048,128],"pathfinder":[1024,128],"aan":[4000,128]}
+flash_linear_args={'cifar':[1024,128],"imdb":[4096,128],"listops":[2048,128],"pathfinder":[1024,128],"aan":[4000,128]}
+lg_args=[8,8,64]
+ls_args={'cifar':[8,8,1024],"imdb":[8,8,4096],"listops":[8,8,2048],"pathfinder":[8,8,1024],"aan":[8,8,4000]}
+performer_args=[8,32]
+# n_layers, d_model
+arch_args={'cifar':
+                {"s4":{"flash":[11,512],"flash_linear":[11,512],"lg":[6,512],"ls":[6,512]},
+                "lra":{"flash":[6,64],"flash_linear":[6,64],"lg":[4,64],"ls":[4,64]}},
+            'listops':
+                {"s4":{"flash":[10,128],"flash_linear":[10,128],"lg":[6,128],"ls":[6,128]},
+                "lra":{"flash":[11,512],"flash_linear":[11,512],"lg":[6,512],"ls":[6,512]}},
+            'pathfinder':
+                {"s4":{"flash":[10,256],"flash_linear":[10,256],"lg":[6,256],"ls":[6,256]},
+                "lra":{"flash":[6,128],"flash_linear":[6,128],"lg":[6,128],"ls":[6,128]}},
+            'imdb':
+                {"s4":{"flash":[5,64],"flash_linear":[5,64],"lg":[4,64],"ls":[4,64]},
+                "lra":{"flash":[11,512],"flash_linear":[11,512],"lg":[6,512],"ls":[6,512]}},
+            'aan':
+                {"s4":{"flash":[10,256],"flash_linear":[10,256],"lg":[6,256],"ls":[6,256]},
+                "lra":{"flash":[6,128],"flash_linear":[6,128],"lg":[4,128],"ls":[4,128]}}
+                }
+
+
+for i, arch in enumerate(archs):
+    for j, task in enumerate(tasks):
+        # time.sleep(30)
+        pid = os.fork()
+        if pid == 0:
+            name = f"{arch}_{task}"
+            print(name)
+            # TODO check imdb task
+            if task == 'imdb':
+                seq_len = 4096
+                args=flash_args[task]
+                if arch == 'flash':
+                    os.system(f'sh /mnt/cache/hanxiaodong/lra/run_task.sh 8 {arch} {name} 0.0005 0.0 MMG')
+                if arch == 'flash_linear':
+                    flash_linear_max_position_embeddings = 4096
+                    flash_linear_s = 128
+                if arch == 'lg':
+                    lg_local_heads = 8
+                    lg_linear_heads = 8
+                    lg_local_chunk_size = 64
+                if arch == 'ls':
+                    ls_attn_heads = 8
+                    ls_attn_window_size = 8
+                    ls_attn_max_seq_len = 4096
+                if arch == 'performer':
+                    performer_heads = 8
+                    performer_approx_attn_dim = 32
+# TODO check cifar task
+            elif task == 'cifar':
+                            seq_len = 1024
+                if arch == 'flash':
+                    flash_max_position_embed = 1024
+                if arch == 'flash_linear':
+                    flash_linear_max_position_embeddings = 1024
+                    flash_linear_s = 128
+                if arch == 'lg':
+                    lg_local_heads = 8
+                    lg_linear_heads = 8
+                    lg_local_chunk_size = 64
+                if arch == 'ls':
+                    ls_attn_heads = 8
+                    ls_attn_window_size = 8
+                    ls_attn_max_seq_len = 1024
+                if arch == 'performer':
+                    performer_heads = 8
+                    performer_approx_attn_dim = 32
+            # TODO check listops task
+            elif task == 'listops':
+                seq_len = 2048
+                if arch == 'flash':
+                    flash_max_position_embed = 2048
+                if arch == 'flash_linear':
+                    flash_linear_max_position_embeddings = 2048
+                    flash_linear_s = 128
+                if arch == 'lg':
+                    lg_local_heads = 8
+                    lg_linear_heads = 8
+                    lg_local_chunk_size = 64
+                if arch == 'ls':
+                    ls_attn_heads = 8
+                    ls_attn_window_size = 8
+                    ls_attn_max_seq_len = 2048
+                if arch == 'performer':
+                    performer_heads = 8
+                    performer_approx_attn_dim = 32
+            # TODO check pathfinder task
+            elif task == 'pathfinder':
+                seq_len = 1024
+                if arch == 'flash':
+                    flash_max_position_embed = 1024
+                if arch == 'flash_linear':
+                    flash_linear_max_position_embeddings = 1024
+                    flash_linear_s = 128
+                if arch == 'lg':
+                    lg_local_heads = 8
+                    lg_linear_heads = 8
+                    lg_local_chunk_size = 64
+                if arch == 'ls':
+                    ls_attn_heads = 8
+                    ls_attn_window_size = 8
+                    ls_attn_max_seq_len = 1024
+                if arch == 'performer':
+                    performer_heads = 8
+                    performer_approx_attn_dim = 32
+            # TODO check aan task
+            elif task == 'aan':
+                seq_len = 4000
+                if arch == 'flash':
+                    flash_max_position_embed = 4000
+                if arch == 'flash_linear':
+                    flash_linear_max_position_embeddings = 4000
+                    flash_linear_s = 128
+                if arch == 'lg':
+                    lg_local_heads = 8
+                    lg_linear_heads = 8
+                    lg_local_chunk_size = 64
+                if arch == 'ls':
+                    ls_attn_heads = 8
+                    ls_attn_window_size = 8
+                    ls_attn_max_seq_len = 4000
+                if arch == 'performer':
+                    performer_heads = 8
+                    performer_approx_attn_dim = 32
+            
+            # os.system(f'bash /mnt/lustre/qinzhen/experiment/nlp/roberta_m3t.sh 8 {arch} {name} 0.0005 0.0 MMG')
+            # sys.exit(0)
